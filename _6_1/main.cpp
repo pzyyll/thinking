f5b52c322304a2ba1e7168ad9be0e957a432b727
caizhili <pzyyll@gmail.com>
2017-06-13 23:17:23 +0800

    fin

--- _6_1/main.cpp
+++ _6_1/main.cpp
@@ -1,77 +1,87 @@
 #include <iostream>
+#include <fstream>
+#include <cstring>
 
 using namespace std;
 
+ifstream fin("input.txt");
+#define cin fin
+
 typedef unsigned short arrele_t;
-const unsigned int MAXN = 250000;
+const unsigned int MAXN = 250000 + 1000;
 arrele_t usA[MAXN];
-unsigned int cnt;
+arrele_t tempA[MAXN];
 
-struct SArr {
-  arrele_t *usA;
-  unsigned int len;
-};
-
-void merge(arrele_t *arr, int p, int q, int r)
-{
+int merge(arrele_t *arr, int p, int q, int r) {
   int llen = q - p + 1;
   int rlen = r - q;
-
-  arrele_t *larr = new arrele_t[llen + 1];
-  arrele_t *rarr = new arrele_t[rlen + 1];
+  int cnt = 0;
+  arrele_t *larr = new arrele_t[llen];
+  arrele_t *rarr = new arrele_t[rlen];
 
   for (int i = 0; i < llen; ++i) {
-      larr[i] = arr[p + i];
+    larr[i] = arr[p + i];
   }
-  larr[llen] = static_cast<arrele_t>(-1);
   for (int i = 0; i < rlen; ++i) {
-      rarr[i] = arr[q + i + 1];
-  }
-  rarr[rlen] = static_cast<arrele_t>(-1);
-
-  int il = 0, ir = 0;
-  for (int i = p; i <= r; ++i) {
-      if (larr[il] > rarr[ir]) {
-          arr[i] = rarr[ir++];
-          ++cnt;
-      } else {
-          arr[i] = larr[il++];
-      }
+    rarr[i] = arr[q + i + 1];
   }
 
-  //delete[] larr;
-  //delete[] rarr;
+  int il = 0, ir = 0, ik = p;
+  for (; il < llen && ir < rlen; ++ik) {
+    if (larr[il] > rarr[ir]) {
+      arr[ik] = rarr[ir++];
+      cnt = cnt + (llen - il);
+    } else {
+      arr[ik] = larr[il++];
+    }
+  }
+
+  while (il < llen) {
+    arr[ik++] = larr[il++];
+  }
+
+  while (ir < rlen) {
+    arr[ik++] = rarr[ir++];
+  }
+
+  delete[] larr;
+  delete[] rarr;
+
+  return cnt;
 }
 
-void merge_sort(arrele_t *arr, int p, int r) {
+int merge_sort_(arrele_t *arr, int p, int r) {
+  int n = 0;
   if (p < r) {
     int m = p + (r - p) / 2;
-    merge_sort(arr, p, m);
-    merge_sort(arr, m + 1, r);
-    merge(arr, p, m, r);
+    n = merge_sort_(arr, p, m);
+    n += merge_sort_(arr, m + 1, r);
+    n += merge(arr, p, m, r);
   }
+
+  return n;
+}
+
+int count_inver(arrele_t *arr, int size_) {
+  memcpy(tempA, arr, size_ * sizeof(arrele_t));
+  return merge_sort_(tempA, 0, size_ - 1);
 }
 
 int main() {
   int n = 0;
   cin >> n;
   for (int i = 0; i < n; ++i) {
-      cin >> usA[i];
+    cin >> usA[i];
   }
 
-  merge_sort(usA, 0, n - 1);
-  cout << cnt << endl;
-//  int m = 0;
-//  cin >> m;
-//  for (int i = 0; i < m; ++i) {
-//    unsigned short x, y;
-//    cin >> x >> y;
-//    usA[x - 1] = y;
-//
-//    merge_sort(usA, 0, n - 1);
-//    cout << cnt << endl;
-//  }
+  int m = 0;
+  cin >> m;
+  for (int i = 0; i < m; ++i) {
+    unsigned short x, y;
+    cin >> x >> y;
+    usA[x - 1] = y;
 
-
+    cout << count_inver(usA, n) << endl;
+  }
   return 0;
 }
